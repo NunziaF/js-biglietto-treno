03a59f2b7647ffa69380627a34277e9c86b0c85e
Nunzia <nunzia.fina@gmail.com>
2020-03-19 17:37:35 +0100

    Calcolo costo biglietto

---
+++ main.js
@@ -0,0 +1,28 @@
+//calcolo biglietto del treno con relativi sconti diversi per le fasce d'età previste
+
+
+// chiedo all'utente di inserire quanti km deve percorrere
+var km = prompt("Quanti km vuoi percorrere?");
+
+// chiedo all'utente di inserire quanti anni ha
+var anni = prompt("Quanti anni hai?");
+
+// moltiplico i km per il costo di 0.21€ al km (mi salvo il resultato)
+var costo = km * 0.21;
+console.log(costo);
+
+// calcolo il costo dei biglietti con lo sconto per minorenni e anziani
+var costomin = costo * 0.80;
+var costoanz = costo * 0.60;
+
+//metto in relazione l'età dell'utente con il costo del biglietto
+if (anni < 18){
+  msg = "Il costo del suo biglietto è " + costomin + "€";
+} else if (anni > 65){
+  msg = "Il costo del suo biglietto è " + costoanz + "€";
+} else if (anni >=18 && anni <=65){
+  msg = "Il costo del suo biglietto è " + costo + "€";
+}
+
+// output costo biglietto per l'utente in base all'età
+document.getElementById('biglietto').innerHTML = msg;
